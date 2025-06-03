c968ecf0b8002cc385ce5127c2b249162eebf48f
LeoBarga <leo.b200411@gmail.com>
2025-06-03 12:53:14 +0200

    Conclusione codice

--- assemblyvuln.asm
+++ assemblyvuln.asm
@@ -23,3 +23,18 @@ my_function:
     mov esp, ebp
     pop ebp
     ret
+
+fine:
+    ; se il programma finisse normalmente
+    mov eax, 1        ;
+    xor ebx, ebx      ; exit code = 0
+    int 0x80
+
+uscita:
+    mov ex, 1
+    xor ebx, ebx
+    int 0x80
+
+SECTION .data
+msg db "Overflow riuscito!\n", 0x0
+msglen equ $ - msg
